dcf1cca546be01226f385dbf94bf3e1f70f70083
Simon Hylander <simon.hylander@skoglit.se>
2023-02-02 23:07:09 +0100

    transitions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+			  type: 'type 1s ease-out .8s alternate both',
+			},
+			keyframes: {
+			  type: {
+				'0%': { transform: 'translateX(0ch)' },
+				'5%, 10%': { transform: 'translateX(1ch)' },
+				'15%, 20%': { transform: 'translateX(2ch)' },
+				'25%, 30%': { transform: 'translateX(3ch)' },
+				'35%, 40%': { transform: 'translateX(4ch)' },
+				'45%, 50%': { transform: 'translateX(5ch)' },
+				'55%, 60%': { transform: 'translateX(6ch)' },
+				'65%, 70%': { transform: 'translateX(7ch)' },
+				'75%, 80%': { transform: 'translateX(8ch)' },
+				'85%, 90%': { transform: 'translateX(9ch)' },
+				'95%, 100%': { transform: 'translateX(11ch)' },
+				'97%, 100%': { transform: 'translateX(13ch)' },
+				'98%, 100%': { transform: 'translateX(15ch)' },
+			  },
+			},
+		  },
 	},
 	plugins: [require('prettier-plugin-tailwindcss')],
 }
