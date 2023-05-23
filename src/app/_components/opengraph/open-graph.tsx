5322053e807f8ac910c604c1b9e23c4a08e49950
Simon Hylander <simon.hylander@skoglit.se>
2023-05-23 10:45:40 +0200

    og img

--- src/app/_components/opengraph/open-graph.tsx
+++ src/app/_components/opengraph/open-graph.tsx
@@ -11,8 +11,11 @@ const OpenGraph = () => {
         property="og:description"
         content="Senior Fullstack developer at Skoglit"
       />
-      <meta property="og:image" content="https://hylander.dev/logo.svg" />
-      <meta property="twitter:card" content="summary" />
+      <meta
+        property="og:image"
+        content="https://hylander.dev/simonhylander.png"
+      />
+      <meta property="twitter:card" content="summary_large_image" />
       <meta
         property="twitter:title"
         content="Simon Hylander - Fullstack Developer"
@@ -21,7 +24,10 @@ const OpenGraph = () => {
         property="twitter:description"
         content="Simon is a seasoned professional at Skoglit, where he solves intricate business challenges through software development."
       />
-      <meta property="twitter:image" content="https://hylander.dev/logo.svg" />
+      <meta
+        property="twitter:image"
+        content="https://hylander.dev/simonhylander.png"
+      />
       <meta property="twitter:site" content="@simon_hylander" />
     </>
   );
